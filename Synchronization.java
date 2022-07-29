206c4821607fcc784ee7145cd795ef2c5fb00c4f
Mirudula12 <mirudula12@gmail.com>
2022-07-30 03:10:52 +0530

    Added

---
+++ Synchronization.java
@@ -0,0 +1,44 @@
+class Table{  
+ synchronized void printTable(int n) {  
+   for(int i=1;i<=4;i++){  
+     System.out.println(i*n);  
+     try{  
+      Thread.sleep(2000);  
+     }
+     catch(Exception e) {
+    	 System.out.println(e);
+     }  
+   }  
+  }  
+}  
+  
+class Thread1 extends Thread {  
+	Table t;  
+	Thread1(Table t){  
+		this.t=t;  
+	}  
+	public void run(){  
+		t.printTable(2);  
+	}  
+	  
+}  
+class Thread2 extends Thread{  
+	Table t;  
+	Thread2(Table t){  
+		this.t=t;  
+	}  
+	
+	public void run(){ 
+		t.printTable(6); 
+	}  
+}  
+  
+public class Synchronization {  
+	public static void main(String args[]){  
+		Table obj = new Table();  
+		Thread1 t1=new Thread1(obj);  
+		Thread2 t2=new Thread2(obj);  
+		t1.start();  
+		t2.start();  
+        }  
+}  
