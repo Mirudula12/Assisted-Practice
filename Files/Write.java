d58954f0e64d3739db487ca0e42c13b9aba95938
Mirudula12 <mirudula12@gmail.com>
2022-07-30 17:38:24 +0530

    Updated

--- Files/Write.java
+++ Files/Write.java
@@ -1,13 +1,11 @@
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.Scanner;
 
-public class File1 {
+public class File2 {
 
 	public static void main(String[] args) throws IOException {
-		
-		FileOutputStream fo = new FileOutputStream("input",true);
+		FileWriter fo = new FileWriter("input",true);
 		if(fo!=null) {
 			System.out.println("File created and opened in write mode ");
 		}
@@ -16,8 +14,8 @@ public class File1 {
 		System.out.println("Enter input to the file:");
 		String fileinput = sc.nextLine();
 		
-		byte b[] = fileinput.getBytes();
-		fo.write(b);
+		//byte b[] = fileinput.getBytes();
+		fo.write(fileinput);
 		System.out.println("Write completed");
 		fo.close();
 
